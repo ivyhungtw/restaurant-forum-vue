1764f433bf358e1f1ccf81ece7f5c0ff55f18252
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-08-01 11:12:29 -0500

    feat: register moment.fromNow() as global filter

--- src/components/LatestRestaurant.vue
+++ src/components/LatestRestaurant.vue
@@ -12,7 +12,7 @@
         <p>
           {{ restaurant.description }}
         </p>
-        {{ restaurant.createdAt }}
+        {{ $filter.fromNow(restaurant.createdAt) }}
         <hr />
       </div>
     </div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue';
+import moment from 'moment';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+
+app.config.globalProperties.$filter = {
+  fromNow(value) {
+    if (!value) return '-';
+    return moment(value).fromNow();
+  }
+};
+app.use(store);
+app.use(router);
+
+app.mount('#app');
