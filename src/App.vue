81415bb2a872067c6c6cb62b91bcbd5a4548acaa
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-07-31 21:25:54 -0500

    feat: add nav bar component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="restaurant-forum">
-    <router-view />
+    <navbar />
+    <main class="mt-5 bg-white">
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import Navbar from './components/Navbar.vue';
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 };
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,90 @@
+<template>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">
+        <i class="fas fa-tshirt fa-lg me-1"></i>
+        Restaurant Forum
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <!-- is user is login -->
+          <template v-if="isAuthenticated">
+            <!-- is user is admin -->
+            <li class="nav-item me-3" v-if="currentUser.isAdmin">
+              <router-link class="nav-link active" aria-current="page" to="#">
+                ADMIN
+              </router-link>
+            </li>
+            <!-- is user is not admin -->
+            <li class="nav-item me-3" v-if="!currentUser.isAdmin">
+              <router-link to="#" class="text-white nav-link">
+                Hi, {{ currentUser.name || 'User' }}
+              </router-link>
+            </li>
+            <!-- Logout button -->
+            <button type="button" class="btn btn-sm btn-outline-primary my-2 my-sm-0">
+              LOGOUT
+            </button>
+          </template>
+
+          <!-- Login button -->
+          <button type="button" class="btn btn-sm btn-outline-primary my-2 my-sm-0" v-else>
+            LOGIN
+          </button>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: '管理者',
+    email: 'root@example.com',
+    image: 'https://i.pravatar.cc/300',
+    isAdmin: false
+  },
+  isAuthenticated: false
+};
+
+export default {
+  data() {
+    return {
+      currentUser: {
+        id: -1,
+        name: '',
+        email: '',
+        image: '',
+        isAdmin: false
+      },
+      isAuthenticated: false
+    };
+  },
+  methods: {
+    fetchUser() {
+      this.currentUser = {
+        ...this.currentUser,
+        ...dummyUser.currentUser
+      };
+      this.isAuthenticated = dummyUser.isAuthenticated;
+    }
+  },
+  created() {
+    this.fetchUser();
+  }
+};
+</script>
