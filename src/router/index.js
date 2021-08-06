74d65319424a36c58bfff388050dbd0c3096d93e
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-08-06 11:30:45 -0500

    chore: add vue config file

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Signin from '@/views/Signin.vue';
 import Restaurants from '@/views/Restaurants.vue';
 import store from '../store';
@@ -117,7 +117,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
   linkExactActiveClass: 'active'
 });
